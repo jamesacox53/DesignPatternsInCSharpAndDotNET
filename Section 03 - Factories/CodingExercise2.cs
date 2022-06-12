f07ac24ef36e94c85d99ad328964b6cc8f38caaf
jamesacox53 <jamesacox53@gmail.com>
2022-06-12 18:52:54 +0100

    Completed Section 3 - Factories

---
+++ Section 03 - Factories/CodingExercise2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03Factories
+{
+    internal class CodingExercise2
+    {
+        static void Main(string[] args)
+        {
+            PersonFactory personFactory = new PersonFactory();
+
+            Person mark = personFactory.CreatePerson("Mark");
+
+            Person geoff = personFactory.CreatePerson("Geoff");
+
+            Console.WriteLine(mark);
+            Console.WriteLine(geoff);
+            Console.ReadKey();
+        }
+    }
+}
