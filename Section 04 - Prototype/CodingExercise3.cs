6fcbbb0e9a3f9eb2456698abaa818038ae614b28
jamesacox53 <jamesacox53@gmail.com>
2022-06-13 14:09:46 +0100

    Completed Section 4 - Prototype

---
+++ Section 04 - Prototype/CodingExercise3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04Prototype
+{
+    internal class CodingExercise3
+    {
+        static void Main(string[] args)
+        {
+            Exercise.Line originalLine = new Exercise.Line();
+
+            Exercise.Point originalStart = new Exercise.Point();
+            originalStart.X = 50;
+            originalStart.Y = 78;
+
+            Exercise.Point originalEnd = new Exercise.Point();
+            originalEnd.X = 140;
+            originalEnd.Y = 221;
+
+
+            originalLine.Start = originalStart;
+            originalLine.End = originalEnd;
+        }
+    }
+}
---
+++ Section 04 - Prototype/Exercise.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04Prototype
+{
+    public class Exercise
+    {
+        public class Point
+        {
+            public int X, Y;
+
+            public Point DeepCopy()
+            {
+                Point newPoint = new Point();
+                newPoint.X = X;
+                newPoint.Y = Y;
+                return newPoint;
+            }
+        }
+
+        public class Line
+        {
+            public Point Start, End;
+
+            public Line DeepCopy()
+            {
+                Line newLine = new Line();
+            
+                Point newStart = Start.DeepCopy();   
+                newLine.Start = newStart;
+
+                Point newEnd = End.DeepCopy();
+                newLine.End = newEnd;
+
+                return newLine;
+            }
+        }
+    }
+}
