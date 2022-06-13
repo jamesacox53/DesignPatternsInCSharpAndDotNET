daf7450f003999fa3074e877b8be6dd73d5f85ee
jamesacox53 <jamesacox53@gmail.com>
2022-06-13 15:44:11 +0100

    Completed Section 05 - Singleton

---
+++ Section 05 - Singleton/CodingExercise4.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section05Singleton
+{
+    internal class CodingExercise4
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Section 05 - Singleton/SingletonTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section05Singleton
+{
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            object obj1 = func.Invoke();
+
+            object obj2 = func.Invoke();
+
+            return obj1 == obj2;
+        }
+    }
+}
