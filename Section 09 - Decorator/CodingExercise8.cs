3866a941af2461281a2b09d4e5b4885fdf41666e
jamesacox53 <jamesacox53@gmail.com>
2022-06-15 18:33:32 +0100

    Completed Section 09 - Decorator

---
+++ Section 09 - Decorator/CodingExercise8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section09Decorator
+{
+    internal class CodingExercise8
+    {
+        static void Main(string[] args)
+        {
+            Dragon dragon = new Dragon();
+            dragon.Age = 30;
+
+            Console.WriteLine(dragon.Fly());
+            Console.WriteLine(dragon.Crawl());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 09 - Decorator/Dragon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section09Decorator
+{
+    public class Bird
+    {
+        public int Age { get; set; }
+
+        public string Fly()
+        {
+            return (Age < 10) ? "flying" : "too old";
+        }
+    }
+
+    public class Lizard
+    {
+        public int Age { get; set; }
+
+        public string Crawl()
+        {
+            return (Age > 1) ? "crawling" : "too young";
+        }
+    }
+
+    public class Dragon // no need for interfaces
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                age = value;
+                bird.Age = age;
+                lizard.Age = age;
+            }
+        }
+
+        public string Fly()
+        {
+            return bird.Fly();
+        }
+
+        public string Crawl()
+        {
+            return lizard.Crawl();
+        }
+    }
+}
