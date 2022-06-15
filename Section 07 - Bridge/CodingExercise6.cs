f3abe2c26d35f901810ce51022b49719fd28dc22
jamesacox53 <jamesacox53@gmail.com>
2022-06-15 10:26:29 +0100

    Completed Section 07 - Bridge

---
+++ Section 07 - Bridge/CodingExercise6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section07Bridge
+{
+    internal class CodingExercise6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Triangle(new RasterRenderer()).ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Section 07 - Bridge/Shape.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Section07Bridge
+{
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        protected IRenderer _renderer;
+
+        public Shape(IRenderer renderer)
+        {
+            this._renderer = renderer;
+        }
+
+        public override string ToString()
+        {
+            return $"Drawing {this.Name} as {_renderer.WhatToRenderAs}";
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer renderer) : base(renderer)
+        {
+            this.Name = "Triangle";
+        } 
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer renderer) : base(renderer)
+        {
+            this.Name = "Square";
+        } 
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        string IRenderer.WhatToRenderAs 
+        {
+            get 
+            {
+                return "lines";
+            }
+        }
+    }
+
+    public class RasterRenderer : IRenderer
+    {
+        string IRenderer.WhatToRenderAs
+        {
+            get
+            {
+                return "pixels";
+            }
+        }
+    }
+}
