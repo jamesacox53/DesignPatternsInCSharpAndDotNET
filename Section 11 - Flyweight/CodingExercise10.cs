11c628619e91917f42e3c4dfaa0fceb33b989d1c
jamesacox53 <jamesacox53@gmail.com>
2022-06-16 20:43:56 +0100

    Completed Section 11 - Flyweight

---
+++ Section 11 - Flyweight/CodingExercise10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section11Flyweight
+{
+    internal class CodingExercise10
+    {
+        static void Main(string[] args)
+        {
+            Sentence sentence = new Sentence("hello world");
+
+            sentence[1].Capitalize = true;
+
+            Console.WriteLine(sentence);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 11 - Flyweight/Sentence.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section11Flyweight
+{
+    public class Sentence
+    {
+        private string[] words;
+        private Dictionary<int, WordToken> wordTokens = new Dictionary<int, WordToken>();
+        
+        public Sentence(string plainText)
+        {
+            this.words = plainText.Split(' ');
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                if (wordTokens.ContainsKey(index)) 
+                {
+                    return wordTokens[index];
+                }
+
+                WordToken wordToken = new WordToken();
+
+                wordTokens.Add(index, wordToken);
+
+                return wordToken;
+            }
+        }
+
+        public override string ToString()
+        {
+            List<string> returnSentence = new List<string>();
+
+            for(int i = 0; i < words.Length; i++) 
+            {
+                string word = words[i];
+
+                if (!wordTokens.ContainsKey(i))
+                {
+                    returnSentence.Add(word);
+                    continue;
+                }
+
+                WordToken token = wordTokens[i];
+
+                string addString = token.Capitalize ? word.ToUpper() : word;
+
+                returnSentence.Add(addString);
+            }
+
+            return string.Join(" ", returnSentence.ToArray());
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+}
