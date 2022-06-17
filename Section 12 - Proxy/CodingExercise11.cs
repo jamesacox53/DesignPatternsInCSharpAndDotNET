c5febadb9071aa5261409f24d5fbb27188787b52
jamesacox53 <jamesacox53@gmail.com>
2022-06-17 20:05:14 +0100

    Completed Section 12 - Proxy

---
+++ Section 12 - Proxy/CodingExercise11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section12Proxy
+{
+    internal class CodingExercise11
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            ResponsiblePerson responsiblePerson = new ResponsiblePerson(person);
+            responsiblePerson.Age = 15;
+
+            Console.WriteLine(responsiblePerson.Drink());
+            Console.WriteLine(responsiblePerson.Drive());
+            Console.WriteLine(responsiblePerson.DrinkAndDrive());
+
+            responsiblePerson.Age = 22;
+
+            Console.WriteLine(responsiblePerson.Drink());
+            Console.WriteLine(responsiblePerson.Drive());
+            Console.WriteLine(responsiblePerson.DrinkAndDrive());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 12 - Proxy/ResponsiblePerson.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section12Proxy
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public string Drink()
+        {
+            return "drinking";
+        }
+
+        public string Drive()
+        {
+            return "driving";
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "driving while drunk";
+        }
+    }
+
+    public class ResponsiblePerson
+    {
+        private Person person;
+        public ResponsiblePerson(Person person)
+        {
+            this.person = person;
+        }
+
+        public int Age 
+        {
+            set
+            {
+                person.Age = value;
+            } 
+            get
+            {
+                return person.Age;
+            }
+        }
+
+        public string Drink()
+        {
+            if (this.Age < 18)
+            {
+                return "too young";
+            }
+            else
+            {
+                return this.person.Drink();
+            }
+        }
+
+        public string Drive()
+        {
+            if (this.Age < 16)
+            {
+                return "too young";
+            }
+            else
+            {
+                return this.person.Drive();
+            }
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "dead";
+        }
+    }
+}
