7f1529ef8c9588f129015d47ca7e32f94c2e441a
jamesacox53 <jamesacox53@gmail.com>
2022-06-18 12:59:23 +0100

    Completed Section 13 - Chain of Responsibility

---
+++ Section 13 - Chain of Responsibility/CodingExercise12.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section13ChainOfResponsibility
+{
+    internal class CodingExercise12
+    {
+        static void Main(string[] args)
+        {
+            // Test1();
+            // Test2();
+            // Test3();
+            // Test4();
+            Test5();
+        }
+
+        static void Test1()
+        {
+            Game game = new Game();
+            
+            Goblin goblin = new Goblin(game);
+            game.Creatures.Add(goblin);
+
+            Console.WriteLine(goblin);
+            Console.ReadKey();
+        }
+
+        static void Test2()
+        {
+            Game game = new Game();
+            
+            Goblin goblin1 = new Goblin(game);
+            game.Creatures.Add(goblin1);
+
+            Goblin goblin2 = new Goblin(game);
+            game.Creatures.Add(goblin2);
+
+
+            Console.WriteLine("Goblin1: " + goblin1.ToString());
+            Console.WriteLine("Goblin2: " + goblin2.ToString());
+            Console.ReadKey();
+        }
+
+        static void Test3()
+        {
+            Game game = new Game();
+
+            Goblin goblin1 = new Goblin(game);
+            game.Creatures.Add(goblin1);
+
+            Goblin goblin2 = new Goblin(game);
+            game.Creatures.Add(goblin2);
+
+            GoblinKing goblinKing1 = new GoblinKing(game);
+            game.Creatures.Add(goblinKing1);
+
+
+            Console.WriteLine("Goblin1: " + goblin1.ToString());
+            Console.WriteLine("Goblin2: " + goblin2.ToString());
+            Console.WriteLine("GoblinKing1: " + goblinKing1.ToString());
+            Console.ReadKey();
+        }
+
+        static void Test4()
+        {
+            Game game = new Game();
+
+            Goblin goblin1 = new Goblin(game);
+            game.Creatures.Add(goblin1);
+
+            Goblin goblin2 = new Goblin(game);
+            game.Creatures.Add(goblin2);
+
+            GoblinKing goblinKing1 = new GoblinKing(game);
+            game.Creatures.Add(goblinKing1);
+
+            GoblinKing goblinKing2 = new GoblinKing(game);
+            game.Creatures.Add(goblinKing2);
+
+
+            Console.WriteLine("Goblin1: " + goblin1.ToString());
+            Console.WriteLine("Goblin2: " + goblin2.ToString());
+            Console.WriteLine("GoblinKing1: " + goblinKing1.ToString());
+            Console.WriteLine("GoblinKing2: " + goblinKing2.ToString());
+            Console.ReadKey();
+        }
+
+        static void Test5()
+        {
+            Game game = new Game();
+
+            Goblin goblin1 = new Goblin(game);
+            game.Creatures.Add(goblin1);
+
+            Goblin goblin2 = new Goblin(game);
+            game.Creatures.Add(goblin2);
+
+            Console.WriteLine("Goblin1: " + goblin1.ToString());
+
+            game.Creatures.Remove(goblin2);
+
+            Console.WriteLine("Goblin1: " + goblin1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 13 - Chain of Responsibility/Game.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section13ChainOfResponsibility
+{
+    public abstract class Creature
+    {
+        protected string name;
+        protected int baseAttack;
+        protected int baseDefense;
+
+        public int Attack 
+        {
+            get 
+            {
+                return CalculateAttack();
+            }
+
+            set 
+            {
+                baseAttack = value;
+            }
+        }
+        public int Defense
+        {
+            get
+            {
+                return CalculateDefense();
+            }
+
+            set
+            {
+                baseDefense = value;
+            }
+        }
+       
+        protected virtual int CalculateAttack()
+        {
+            return 0;
+        }
+        protected virtual int CalculateDefense()
+        {
+            return 0;
+        }
+
+        public virtual int GetAttackBonus()
+        {
+            return 0;
+        }
+
+        public virtual int GetDefenseBonus()
+        {
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, Attack: {Attack}, Defense: {Defense}";
+        }
+    }
+
+    public class Goblin : Creature
+    {
+        protected Game game;
+        public Goblin(Game game) 
+        {
+            this.name = "Goblin";
+            this.game = game;
+            this.baseAttack = 1;
+            this.baseDefense = 1;
+        }
+
+        protected override int CalculateAttack()
+        {
+            int attack = this.baseAttack;
+
+            foreach(Creature creature in this.game.Creatures)
+            {   
+                if (Object.ReferenceEquals(creature, this)) continue;
+                
+                attack += creature.GetAttackBonus();
+            }
+
+            return attack;
+        }
+
+        protected override int CalculateDefense()
+        {
+            int defense = this.baseDefense;
+
+            foreach (Creature creature in this.game.Creatures)
+            { 
+                if (Object.ReferenceEquals(creature, this)) continue;
+
+                defense += creature.GetDefenseBonus();
+            }
+
+            return defense;
+        }
+
+        public override int GetAttackBonus()
+        {
+            return 0;
+        }
+
+        public override int GetDefenseBonus()
+        {
+            return 1;
+        }
+    }
+
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) : base(game)
+        {
+            this.name = "Goblin King";
+            this.baseAttack = 3;
+            this.baseDefense = 3;
+        }
+
+        public override int GetAttackBonus()
+        {
+            return 1;
+        }
+    }
+
+    public class Game
+    {
+        public IList<Creature> Creatures = new List<Creature>();
+    }
+}
