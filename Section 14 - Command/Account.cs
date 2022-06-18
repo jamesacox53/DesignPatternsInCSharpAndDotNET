be81125a32be12d3c061df16336515c4aeece03d
jamesacox53 <jamesacox53@gmail.com>
2022-06-18 14:38:11 +0100

    Completed Section 14 - Command

---
+++ Section 14 - Command/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section14Command
+{
+    public class Command
+    {
+        public enum Action
+        {
+            Deposit,
+            Withdraw
+        }
+
+        public Action TheAction;
+        public int Amount;
+        public bool Success;
+    }
+
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public void Process(Command c)
+        {
+            if (c.TheAction == Command.Action.Deposit)
+            {
+                bool success = Deposit(c.Amount);
+                c.Success = success;
+            }
+            else if (c.TheAction == Command.Action.Withdraw)
+            {
+                bool success = Withdraw(c.Amount);
+                c.Success = success;
+            }
+        }
+
+        private bool Deposit(int amount)
+        {
+            if (amount < 0) return false;
+            
+            Balance += amount;
+            
+            return true;
+        }
+
+        private bool Withdraw(int amount)
+        {
+            if (amount < 0 || (Balance - amount) < 0) return false;
+
+            Balance -= amount;
+
+            return true;
+        }
+    }
+}
---
+++ Section 14 - Command/CodingChallenge13.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section14Command
+{
+    internal class CodingChallenge13
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+
+            Console.WriteLine(account.Balance);
+
+            Command command1 = new Command();
+            command1.Amount = 100;
+            command1.TheAction = Command.Action.Deposit;
+            
+            account.Process(command1);
+
+            Console.WriteLine(account.Balance);
+
+            Command command2 = new Command();
+            command2.Amount = 50;
+            command2.TheAction = Command.Action.Withdraw;
+
+            account.Process(command2);
+
+            Console.WriteLine(account.Balance);
+            Console.ReadKey();
+        }
+
+        
+    }
+}
