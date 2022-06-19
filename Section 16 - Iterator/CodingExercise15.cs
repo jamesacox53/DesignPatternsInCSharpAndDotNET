f26dc31f3147007fa2e98d7713b224837efcb3a3
jamesacox53 <jamesacox53@gmail.com>
2022-06-19 14:40:02 +0100

    Completed Section 16 - Iterator

---
+++ Section 16 - Iterator/CodingExercise15.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section16Iterator
+{
+    internal class CodingExercise15
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        static void Test1()
+        {
+            var node = new Node<char>('a',
+          new Node<char>('b',
+            new Node<char>('c'),
+            new Node<char>('d')),
+          new Node<char>('e'));
+
+            string preOrderTraversal = new string(node.PreOrder.ToArray());
+
+            Console.WriteLine($"PreOrder Traversal is: {preOrderTraversal} and should be \"abcde\"");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 16 - Iterator/Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section16Iterator
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+
+        public IEnumerable<T> PreOrder
+        {
+            get
+            {
+                foreach (var node in TraverseInPreOrder(this))
+                    yield return node.Value;
+            }
+        }
+    
+        private IEnumerable<Node<T>> TraverseInPreOrder(Node<T> current)
+        {
+            yield return current;
+
+            if (current.Left != null)
+            {
+                foreach (var left in TraverseInPreOrder(current.Left))
+                    yield return left;
+            }
+            
+            if (current.Right != null)
+            {
+                foreach (var right in TraverseInPreOrder(current.Right))
+                    yield return right;
+            }
+        }
+    }
+}
