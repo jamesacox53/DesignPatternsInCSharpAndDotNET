58855300cb381396b987f51b27409f5a3ed03421
jamesacox53 <jamesacox53@gmail.com>
2022-06-19 20:47:48 +0100

    Completed Section 19 - Null Object

---
+++ Section 19 - Null Object/CodingExercise18.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section19NullObject
+{
+    internal class CodingExercise18
+    {
+        static void Main(string[] args)
+        {
+            // SingleCallTest();
+            ManyCallsTest();
+        }
+
+        public static void SingleCallTest()
+        {
+            var a = new Account(new NullLog());
+            a.SomeOperation();
+            Console.ReadKey();
+        }
+
+        public static void ManyCallsTest()
+        {
+            var a = new Account(new NullLog());
+            for (int i = 0; i < 100; ++i)
+                a.SomeOperation();
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Section 19 - Null Object/NullLog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section19NullObject
+{
+    public interface ILog
+    {
+        // maximum # of elements in the log
+        int RecordLimit { get; }
+
+        // number of elements already in the log
+        int RecordCount { get; set; }
+
+        // expected to increment RecordCount
+        void LogInfo(string message);
+    }
+
+    public class Account
+    {
+        private ILog log;
+
+        public Account(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void SomeOperation()
+        {
+            int c = log.RecordCount;
+            log.LogInfo("Performing an operation");
+            if (c + 1 != log.RecordCount)
+                throw new Exception();
+            if (log.RecordCount >= log.RecordLimit)
+                throw new Exception();
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        private int recordCount;
+
+        public int RecordLimit 
+        { 
+            get 
+            {
+                return int.MaxValue;
+            }
+        }
+
+        public int RecordCount
+        {
+            get
+            {
+                return recordCount;
+            }
+            set
+            {
+                recordCount = value;
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            recordCount++;
+        }
+    }
+}
