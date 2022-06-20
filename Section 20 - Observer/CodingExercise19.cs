d50aa9efac1fef5cc0a6ef2780f4fc205c1ada87
jamesacox53 <jamesacox53@gmail.com>
2022-06-20 16:53:09 +0100

    Completed Section 20 - Observer

---
+++ Section 20 - Observer/CodingExercise19.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section20Observer
+{
+    internal class CodingExercise19
+    {
+        static void Main(string[] args)
+        {
+            // SingleRatTest();
+            // TwoRatTest();
+            ThreeRatsOneDies();
+        }
+       
+        public static void SingleRatTest()
+        {
+            var game = new Game();
+            var rat = new Rat(game);
+            Console.WriteLine($"Rat Attack is {rat.Attack} and should be 1");
+            Console.ReadKey();
+        }
+        
+        public static void TwoRatTest()
+        {
+            var game = new Game();
+            var rat1 = new Rat(game);
+            var rat2 = new Rat(game);
+            Console.WriteLine($"Rat1 Attack is {rat1.Attack} and should be 2");
+            Console.WriteLine($"Rat2 Attack is {rat2.Attack} and should be 2");
+            Console.ReadKey();
+        }
+
+        public static void ThreeRatsOneDies()
+        {
+            var game = new Game();
+
+            var rat1 = new Rat(game);
+            Console.WriteLine($"Rat1 Attack is {rat1.Attack} and should be 1");
+
+            var rat2 = new Rat(game);
+            Console.WriteLine($"Rat1 Attack is {rat1.Attack} and should be 2");
+            Console.WriteLine($"Rat2 Attack is {rat2.Attack} and should be 2");
+            
+            using (var rat3 = new Rat(game))
+            {
+                Console.WriteLine($"Rat1 Attack is {rat1.Attack} and should be 3");
+                Console.WriteLine($"Rat2 Attack is {rat2.Attack} and should be 3");
+                Console.WriteLine($"Rat3 Attack is {rat3.Attack} and should be 3");
+            }
+
+            Console.WriteLine($"Rat1 Attack is {rat1.Attack} and should be 2");
+            Console.WriteLine($"Rat2 Attack is {rat2.Attack} and should be 2");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 20 - Observer/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section20Observer
+{
+    public class Game
+    {
+        public event EventHandler ResetRatAttack;
+        public event EventHandler IncludeRatInAttackCalculation;
+        public event EventHandler IncrementAttack;
+
+        public void AddRatToEventHandlers(Rat rat)
+        {
+            this.ResetRatAttack += rat.Game_ResetRatAttack;
+            this.IncludeRatInAttackCalculation += rat.Game_IncludeRatInAttackCalculation;
+            this.IncrementAttack += rat.Game_IncrementAttack;
+        }
+
+        public void RemoveRatFromEventHandlers(Rat rat)
+        {
+            this.ResetRatAttack -= rat.Game_ResetRatAttack;
+            this.IncludeRatInAttackCalculation -= rat.Game_IncludeRatInAttackCalculation;
+            this.IncrementAttack -= rat.Game_IncrementAttack;
+        }
+
+        public void RatCreatedInGame(Rat rat)
+        {
+            ResetRatAttack?.Invoke(rat, EventArgs.Empty);
+
+            CalculateRatAttack(rat);
+        }
+
+        public void CalculateRatAttack(Rat rat)
+        {
+            IncludeRatInAttackCalculation?.Invoke(rat, EventArgs.Empty);
+        }
+
+        public void IncrementRatAttack(Rat rat)
+        {
+            IncrementAttack?.Invoke(rat, EventArgs.Empty);
+        }
+
+        public void RatDiedInGame(Rat rat)
+        {
+            ResetRatAttack?.Invoke(rat, EventArgs.Empty);
+
+            CalculateRatAttack(rat);
+        }
+    }
+
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+        private Game game;
+
+        public Rat(Game game)
+        {
+            this.game = game;
+            this.game.AddRatToEventHandlers(this);
+
+            this.game.RatCreatedInGame(this);
+        }
+
+        public void Game_ResetRatAttack(object sender, EventArgs e)
+        {
+            Attack = 0;
+        }
+
+        public void Game_IncludeRatInAttackCalculation(object sender, EventArgs e)
+        {
+            this.game.IncrementRatAttack(this);
+        }
+
+        public void Game_IncrementAttack(object sender, EventArgs e)
+        {
+            Attack++;
+        }
+
+        public void Dispose()
+        {
+            this.game.RemoveRatFromEventHandlers(this);
+                
+            this.game.RatDiedInGame(this);
+        }
+    }
+}
