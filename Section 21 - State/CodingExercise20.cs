e474c1f22a250548dfe47780c949551109235765
jamesacox53 <jamesacox53@gmail.com>
2022-06-20 18:45:18 +0100

    Completed Section 21 - State

---
+++ Section 21 - State/CodingExercise20.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section21State
+{
+    internal class CodingExercise20
+    {
+        static void Main(string[] args)
+        {
+            // TestSuccess();
+            TestFailure();
+        }
+
+        public static void TestSuccess()
+        {
+            var cl = new CombinationLock(new[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"LOCKED\"");
+            cl.EnterDigit(1);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"1\"");
+            cl.EnterDigit(2);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"12\"");
+            cl.EnterDigit(3);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"123\"");
+            cl.EnterDigit(4);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"1234\"");
+            cl.EnterDigit(5);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"OPEN\"");
+            Console.ReadKey();
+        }
+
+        public static void TestFailure()
+        {
+            var cl = new CombinationLock(new[] { 1, 2, 3 });
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"LOCKED\"");
+            cl.EnterDigit(1);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"1\"");
+            cl.EnterDigit(2);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"12\"");
+            cl.EnterDigit(5);
+            Console.WriteLine($"Lock status is {cl.Status} and should be \"ERROR\"");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section 21 - State/CombinationLock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section21State
+{
+    public class CombinationLock
+    {
+        private int[] combination;
+        private string combinationString;
+
+        public CombinationLock(int[] combination)
+        {
+            this.combination = combination;
+            this.combinationString = string.Join("", combination.Select(i => i.ToString()));
+            this.Status = "LOCKED";
+        }
+
+        public string Status;
+
+        public void EnterDigit(int digit)
+        {
+            if (this.Status == "ERROR" || this.Status == "OPEN")
+                return;
+
+            if (this.Status == "LOCKED")
+                this.Status = "";
+
+            this.Status += digit;
+
+            if (this.Status == combinationString)
+            {
+                this.Status = "OPEN";
+                return;
+            }
+
+            if (!this.combinationString.StartsWith(Status))
+            {
+                this.Status = "ERROR";
+                return;
+            }
+        }
+    }
+}
