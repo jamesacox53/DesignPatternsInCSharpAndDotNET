a2162a33023eb5e129c5492112b20aaddb96043e
jamesacox53 <jamesacox53@gmail.com>
2022-06-21 20:13:25 +0100

    Completed Section 22 - Strategy

---
+++ Section 22 - Strategy/CodingExercise21.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Section22Strategy
+{
+    internal class CodingExercise21
+    {
+        static void Main(string[] args)
+        {
+            // PositiveTestOrdinaryStrategy();
+            // PositiveTestRealStrategy();
+            // NegativeTestOrdinaryStrategy();
+            NegativeTestRealStrategy();
+        }
+
+        public static void PositiveTestOrdinaryStrategy()
+        {
+            var strategy = new OrdinaryDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 10, 16);
+            Console.WriteLine($"Result1 is {GetComplexNumberString(results.Item1)} and should be {GetComplexNumberString(new Complex(-2, 0))}");
+            Console.WriteLine($"Result2 is {GetComplexNumberString(results.Item2)} and should be {GetComplexNumberString(new Complex(-8, 0))}");
+            Console.ReadKey();
+        }
+
+        public static void PositiveTestRealStrategy()
+        {
+            var strategy = new RealDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 10, 16);
+            Console.WriteLine($"Result1 is {GetComplexNumberString(results.Item1)} and should be {GetComplexNumberString(new Complex(-2, 0))}");
+            Console.WriteLine($"Result2 is {GetComplexNumberString(results.Item2)} and should be {GetComplexNumberString(new Complex(-8, 0))}");
+            Console.ReadKey();
+        }
+
+        public static void NegativeTestOrdinaryStrategy()
+        {
+            var strategy = new OrdinaryDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 4, 5);
+            Console.WriteLine($"Result1 is {GetComplexNumberString(results.Item1)} and should be {GetComplexNumberString(new Complex(-2, 1))}");
+            Console.WriteLine($"Result2 is {GetComplexNumberString(results.Item2)} and should be {GetComplexNumberString(new Complex(-2, -1))}");
+            Console.ReadKey();
+        }
+
+        public static void NegativeTestRealStrategy()
+        {
+            var strategy = new RealDiscriminantStrategy();
+            var solver = new QuadraticEquationSolver(strategy);
+            var results = solver.Solve(1, 4, 5);
+            var complexNaN = new Complex(double.NaN, double.NaN);
+
+            Console.WriteLine($"Result1 is {GetComplexNumberString(results.Item1)} and should be {GetComplexNumberString(complexNaN)}");
+            Console.WriteLine($"Result2 is {GetComplexNumberString(results.Item2)} and should be {GetComplexNumberString(complexNaN)}");
+            Console.ReadKey();
+            /*
+            Assert.IsTrue(double.IsNaN(results.Item1.Real));
+            Assert.IsTrue(double.IsNaN(results.Item1.Imaginary));
+            Assert.IsTrue(double.IsNaN(results.Item2.Real));
+            Assert.IsTrue(double.IsNaN(results.Item2.Imaginary));
+            */
+        }
+
+        private static string GetComplexNumberString(Complex complex)
+        {
+            return $"Real: {complex.Real}, Imag: {complex.Imaginary}";
+        }
+    }
+}
---
+++ Section 22 - Strategy/QuadraticEquationSolver.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Section22Strategy
+{
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            return ((b * b) - (4 * a * c));
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            double discriminant = ((b * b) - (4 * a * c));
+
+            if (discriminant < 0) return double.NaN;
+
+            return discriminant;
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            double discriminant = strategy.CalculateDiscriminant(a, b, c);
+
+            if (discriminant == double.NaN)
+                return NaNTuple();
+
+            Complex complexDiscriminant = new Complex(discriminant, 0);
+            Complex discriminantSqrtd = Complex.Sqrt(complexDiscriminant);
+
+            Complex res1 = ((-b) + discriminantSqrtd) / (2 * a);
+            Complex res2 = ((-b) - discriminantSqrtd) / (2 * a);
+
+            return new Tuple<Complex, Complex>(res1, res2);
+        }
+
+        private Tuple<Complex, Complex> NaNTuple()
+        {
+            Complex res1 = new Complex(double.NaN, double.NaN);
+            Complex res2 = new Complex(double.NaN, double.NaN);
+
+            return new Tuple<Complex, Complex>(res1, res2);
+        }
+    }
+}
