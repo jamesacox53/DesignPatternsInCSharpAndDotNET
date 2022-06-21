edc32e9b3685abb7d0aa11c60c0d19b879d99873
jamesacox53 <jamesacox53@gmail.com>
2022-06-21 20:53:49 +0100

    Completed Section 23 - Template Method

---
+++ Section 23 - Template Method/CardGame.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section23TemplateMethod
+{
+    public class Creature
+    {
+        public int Attack, Health;
+
+        public Creature(int attack, int health)
+        {
+            Attack = attack;
+            Health = health;
+        }
+    }
+
+    public abstract class CardGame
+    {
+        public Creature[] Creatures;
+
+        public CardGame(Creature[] creatures)
+        {
+            Creatures = creatures;
+        }
+
+        // returns -1 if no clear winner (both alive or both dead)
+        public int Combat(int creature1, int creature2)
+        {
+            Creature first = Creatures[creature1];
+            Creature second = Creatures[creature2];
+            Hit(first, second);
+            Hit(second, first);
+            bool firstAlive = first.Health > 0;
+            bool secondAlive = second.Health > 0;
+            if (firstAlive == secondAlive) return -1;
+            return firstAlive ? creature1 : creature2;
+        }
+
+        // attacker hits other creature
+        protected abstract void Hit(Creature attacker, Creature other);
+    }
+
+    public class TemporaryCardDamageGame : CardGame
+    {
+        public TemporaryCardDamageGame(Creature[] creatures) : base(creatures)
+        {
+        }
+
+        protected override void Hit(Creature attacker, Creature other)
+        {
+            int newOtherhealth = other.Health - attacker.Attack;
+
+            other.Health = newOtherhealth <= 0 ? 0 : other.Health;
+        }
+    }
+
+    public class PermanentCardDamage : CardGame
+    {
+        public PermanentCardDamage(Creature[] creatures) : base(creatures)
+        {
+        }
+
+        protected override void Hit(Creature attacker, Creature other)
+        {
+            int newOtherhealth = other.Health - attacker.Attack;
+
+            other.Health = newOtherhealth < 0 ? 0 : newOtherhealth;
+        }
+    }
+}
---
+++ Section 23 - Template Method/CodingExercise22.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section23TemplateMethod
+{
+    internal class CodingExercise22
+    {
+        static void Main(string[] args)
+        {
+            // ImpasseTest();
+            // TemporaryMurderTest();
+            // DoubleMurderTest();
+            PermanentDamageDeathTest();
+        }
+
+        public static void ImpasseTest()
+        {
+            var c1 = new Creature(1, 2);
+            var c2 = new Creature(1, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {-1}");
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {-1}");
+            Console.ReadKey();
+        }
+
+        public static void TemporaryMurderTest()
+        {
+            var c1 = new Creature(1, 1);
+            var c2 = new Creature(2, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {1}");
+            Console.ReadKey();
+        }
+
+        public static void DoubleMurderTest()
+        {
+            var c1 = new Creature(2, 2);
+            var c2 = new Creature(2, 2);
+            CardGame game = new TemporaryCardDamageGame(new[] { c1, c2 });
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {-1}");
+            Console.ReadKey();
+        }
+
+        public static void PermanentDamageDeathTest()
+        {
+            var c1 = new Creature(1, 2);
+            var c2 = new Creature(1, 3);
+            CardGame game = new PermanentCardDamage(new[] { c1, c2 });
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {-1}");
+            Console.WriteLine($"Game Combat was {game.Combat(0, 1)} and should have been {1}");
+            Console.ReadKey();
+        }
+    }
+}
